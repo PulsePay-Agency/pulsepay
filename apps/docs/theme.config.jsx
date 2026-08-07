11b5fb15c089a137864ce27077d0463cf59bf322
Oluwafemi Obayemi <thefemology@gmail.com>
2026-08-07 15:59:51 +0100

    docs(site): add theme.config.jsx

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>PulsePay Documentation</span>,
+  project: {
+    link: 'https://github.com/Femology/pulsepay'
+  },
+  docsRepositoryBase: 'https://github.com/Femology/pulsepay/tree/main/apps/docs',
+}
